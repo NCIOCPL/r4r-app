6277fc91c7c63a64a4d99736054f27aa40cb27eb
Brendan Beltz <bbeltz@sapient.com>
2018-04-16 13:57:34 -0400

    Added in default theme custom checkboxes

--- src/components/CheckFilter.js
+++ src/components/CheckFilter.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Theme } from '../theme';
 import { keyHandler } from '../utilities';
+import './CheckFilter.css';
 
 class CheckFilter extends React.PureComponent {
     static propTypes = {
