1ebc1ad56d5783517323151d0c8a9caf9cd134d0
Brendan Beltz <bbeltz@sapient.com>
2018-05-16 12:11:41 -0400

    NoTouch refactored to functional

--- src/components/NoTouch.js
+++ src/components/NoTouch.js
@@ -1,22 +1,13 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { Theme } from '../theme';
 
-class NoTouch extends React.PureComponent {
-    static propTypes = {
-
-    }
-
-    render(){
-        return(
-            <Theme
-                element="div"
-                className="notouch-overlay"
-            >
-                {this.props.children}
-            </Theme>
-        )
-    }
-}
+const NoTouch = props => (
+    <Theme
+        element="div"
+        className="notouch-overlay"
+    >
+        { props.children }
+    </Theme>
+)
 
 export default NoTouch;
