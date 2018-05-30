2408e0f829c6e252476d9a558e4652b84d0b8b82
Brendan Beltz <bbeltz@sapient.com>
2018-05-30 19:18:23 -0400

    Reducer added to keep record of location history

---
+++ src/state/history/reducer.js
@@ -0,0 +1,15 @@
+// This reducer is simply a patch on the react-router-redux to allow us to save a
+// record of location changes
+
+const initialState = [];
+
+const reducer = (state = initialState, action) => {
+    switch(action.type){
+        case '@@router/LOCATION_CHANGE':
+            return [...state, action.payload];
+        default:
+            return state;
+    }
+}
+
+export default reducer;
--- src/state/index.js
+++ src/state/index.js
@@ -3,3 +3,4 @@ export { default as searchForm } from './searchForm/reducer';
 export { default as announcements } from './announcements/reducer';
 export { default as error } from './error/reducer';
 export { default as cache } from './cache/reducer';
+export { default as history } from './history/reducer';
