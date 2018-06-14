dd373a7c6dd5e6c5e7df8fa954d85122e8914947
Brendan Beltz <bbeltz@sapient.com>
2018-06-14 11:20:08 -0400

    Add Enzyme-context-patch as workaround for testing 16.3 context API

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,4 +1,21 @@
-import { configure } from 'enzyme';
+import React from 'react';
+import { configure, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import { ThemeProvider } from './theme';
+import { StaticRouter } from 'react-router-dom';
+
 
 configure({ adapter: new Adapter() });
+
+// BIG NOTE:
+// Currently (June 2018) enzyme has not been updated to correctly handle the new React ^16.3 context API. 
+// The dependency enzyme-context-patch adds a postinstall hook that lets Enzyme support the API until the official
+// library updates. When it does, future you should be able to remove the patch dependency with no changes to tests. (Fingers crossed).
+
+// This gives us a useful helper function to quickly test components that are 'littered' with Themed components
+global.mountWithTheme = node => mount(<ThemeProvider theme={{}}>{ node }</ThemeProvider>);
+
+// Containers containing Link or Route components need to be wrapped in a router. We use StaticRouter for minimal footprint
+global.mountWithRouter = node => mount(<StaticRouter>{ node }</StaticRouter>);
+
+global.mountWithThemeAndRouter = node => mount(<StaticRouter><ThemeProvider theme={{}}>{ node }</ThemeProvider></StaticRouter>);
