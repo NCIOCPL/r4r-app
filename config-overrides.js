c434acff9ae8acfc45de7baa7c25a7d4078c73a4
Brendan Beltz <bbeltz@sapient.com>
2018-06-16 13:11:36 -0400

    First test of react-app-rewired

---
+++ config-overrides.js
@@ -0,0 +1,29 @@
+// Custom Webpack Plugin to remove CRA filename hashes.
+const NoHashes = {
+    apply: config => {
+        const walk = obj => {
+            const paths = {
+              object: k => walk(obj[k]),
+              string: k => {
+                obj[k] = obj[k].replace(/\[\w*?hash:\d+\]\./, '');
+              },
+            };
+            if (obj != null) {
+              Object.keys(obj)
+                .filter(k => paths[typeof obj[k]])
+                .forEach(k => paths[typeof obj[k]](k));
+            }
+        };
+        walk(config);
+    }
+}
+
+module.exports = function override(config, env){
+    config.output = Object.assign(config.output, {
+        libraryTarget: 'umd',
+        library: 'r4r',
+        umdNamedDefine: true
+    })
+    config.plugins.push(NoHashes)
+    return config;
+};
