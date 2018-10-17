4a6237a9a71826cd50c5832dc744ae1f41ac5412
Brendan Beltz <bbeltz@sapient.com>
2018-10-17 11:19:19 -0400

    Remove enzyme-context-patch now that enzyme adapter has been updated

--- src/setupTests.js
+++ src/setupTests.js
@@ -7,11 +7,6 @@ import { StaticRouter } from 'react-router-dom';
 
 configure({ adapter: new Adapter() });
 
-// BIG NOTE:
-// Currently (June 2018) enzyme has not been updated to correctly handle the new React ^16.3 context API. 
-// The dependency enzyme-context-patch adds a postinstall hook that lets Enzyme support the API until the official
-// library updates. When it does, future you should be able to remove the patch dependency with no changes to tests. (Fingers crossed).
-
 // This gives us a useful helper function to quickly test components that are 'littered' with Themed components
 global.mountWithTheme = node => mount(<ThemeProvider theme={{}}>{ node }</ThemeProvider>);
 
