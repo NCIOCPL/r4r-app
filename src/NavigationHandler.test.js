610872072403594b4831dc3143692827e582b4c1
Brendan Beltz <bbeltz@sapient.com>
2018-10-17 10:50:24 -0400

    Add placeholder window.scrollTo because jsdom doesn't support
    The actual functionality of the API isn't being tested, so an empty placeholder is adequate in this case. If we were testing the behavior of scrollTo we would need to use some form of headless browser testin, such as puppeteer.

--- src/NavigationHandler.test.js
+++ src/NavigationHandler.test.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import { NavigationHandler } from './NavigationHandler';
 
+window.scrollTo = () => {}
+
 describe('NavigationHandler HOC', () => {
 
   it('renders correctly', () => {
