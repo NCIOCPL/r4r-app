0d2a9652a5c90ec90025e3f720fe11e696c2978e
Arce, Paul (NIH/NCI) [C] <arcepe@mail.nih.gov>
2019-10-24 18:18:49 -0400

    fixes from npm audit fix and resolving some js warnings preventing ci builds

--- src/views/home/index.js
+++ src/views/home/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { Helmet } from 'react-helmet';
 import { Theme } from '../../theme';
 import SearchBar from '../../components/SearchBar';
 import BrowseBox from '../../components/BrowseBox';
